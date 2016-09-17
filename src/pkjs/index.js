4530ba8c9b6a5d18056fd214f7c03aa55c7c3987
Elisha Lai <elisha.lai@outlook.com>
2016-09-17 16:13:08 -0400

    minor changes

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,14 +1,25 @@
 // PebbleKit JS (pkjs)
 
+var myAPIKey = '90315628e806c356a01030a77c3077f6';
+
 Pebble.on('message', function(event) {
   // Get the message that was passed
   var message = event.data;
 
   if (message.fetch) {
     navigator.geolocation.getCurrentPosition(function(location) {
+      var url = 'https://developers.zomato.com/api/v2.1/geocode' +
+              '?lat=' + pos.coords.latitude +
+              '&lon=' + pos.coords.longitude +
+              '&user-key=' + myAPIKey;
+
+      request(url, 'GET', function(respText) {
+        var restaurantData = JSON.parse(respText);
+
         Pebble.postMessage({
-          'location': location
+          'restaurantData': restaurantData
         });
+      });
     }, function(err) {
       console.error('Error getting location');
     },
